e0c1709d9bf67a604a61e3c8d7f3908ae7337086
wzajj <>
2019-04-11 08:51:16 +0800

    再次提交

---
+++ Demo/Demo/CoverFlowLayout.h
@@ -0,0 +1,15 @@
+//
+//  CoverFlowLayout.h
+//  BlankProject
+//
+//  Created by mac on 17/7/31.
+//  Copyright © 2017年 Mephsito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define kItemWidth 200
+#define kItemHeight 200
+
+@interface CoverFlowLayout : UICollectionViewFlowLayout
+@property (nonatomic,assign) NSInteger index;
+@end
